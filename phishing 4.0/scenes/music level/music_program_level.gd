ef1374875a01f471506d22ca63f46e00de4964c3
SomeToffee <taiprult@gmail.com>
2023-11-23 15:52:46 +0000

    Clean up and Fade in sequencing

--- phishing 4.0/scenes/music level/music_program_level.gd
+++ phishing 4.0/scenes/music level/music_program_level.gd
@@ -10,38 +10,29 @@ extends Node2D
 var stasis = false
 @onready var original_position = player.position
 var follow = 1
+var program_open = false
 
-#@onready var player = get_tree().get_root().get_node("/root/Player")
-#@onready var playhead = $playhead
-# 
-#var follow = 1
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	playhead.global_position.x = -309
-	#var original_position = player.global_position
-	#stasis = true
-	
-	fade_timer.start()
 
 	$AnimationPlayer.play("fade_in_splash")
+
+
 func _physics_process(delta):
-#	if fade_timer.timeout:
-#		stasis = false
-#	if stasis == true:
-#		player.global_position = original_position
-#	if fade_timer.timeout:
-#		player.move_lock = false
 	print(original_position)
-	#print(player.global_position.y)
-	#print(playhead.global_position.x)
-	splash_screen.position = player.position
-	
-	follow = p_camera.global_position.y
-#	tween.tween_property($playhead, "position", Vector2(11356, follow), 96)
-	playhead.position.x +=  2.0059027
-	playhead.global_position.y = follow
-	if playhead.global_position.x > 17031:
-		playhead.global_position.x = -309
+	if program_open == true:
+		if $level_song.playing == false:
+			$level_song.play()
+		follow = p_camera.global_position.y
+		playhead.position.x +=  2.0059027
+		playhead.global_position.y = follow
+		if playhead.global_position.x > 17031:
+			playhead.global_position.x = -309
 
 
 
+
+
+func _on_animation_player_animation_finished(fade_in_splash):
+	program_open = true
